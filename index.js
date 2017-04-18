a60c52510a27f25519c486d8568ff53f627ccbcf
Jaret Burkett <jaretburkett@gmail.com>
2017-04-18 09:41:18 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var sassMiddleware = require('node-sass-middleware');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+
+var bitcoinPrice = 0;
+var bitcoinHistorical={};
+
+var CoinDesk = require("node-coindesk");
+var coindesk = new CoinDesk();
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'scss'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+}));
+// Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'bower_components')));
+
+
+// websocket stuff
+io.on('connection', function(socket){
+    getPrice();
+    getBitcoinHistorical();
+});
+
+function getBitcoinHistorical() {
+    var start_date = new Date();
+    var end_date = new Date();
+    end_date.setDate(start_date.getDate() - 30);
+    coindesk.historical({start_date: start_date, end_date: end_date}, function (data) {
+        try {
+            data = JSON.parse(data);
+            bitcoinHistorical = data.bpi;
+            console.log(data);
+            io.emit('bitcoinHistorical', bitcoinHistorical);
+        } catch(e){
+
+        }
+
+    });
+}
+
+function getPrice() {
+    coindesk.currentPrice(function (data) {
+        try{
+            data = JSON.parse(data);
+            bitcoinPrice = data.bpi.USD.rate_float.toFixed(2);
+            io.emit('bitcoinPrice', bitcoinPrice);
+        } catch(e){
+
+        }
+    });
+}
+
+// get bitcoin prices
+getPrice();
+getBitcoinHistorical();
+setInterval(function () {
+    getPrice();
+    getBitcoinHistorical();
+}, 60000);
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
---
+++ public/js/main.js
@@ -0,0 +1,105 @@
+var socket = io();
+$(function () {
+    // var socket = io();
+    socket.on('bitcoinPrice', function(bitcoinPrice){
+        $('.bitcoin-price').html(formatNumber(bitcoinPrice));
+        $('.price-sub').html(moment().format('MMMM Do YYYY, h:mm a'));
+    });
+    socket.on('bitcoinHistorical', function(bitcoinHistorical){
+        var newData = [];
+        for (var date in bitcoinHistorical) {
+            var item = {
+                x: date,
+                y: bitcoinHistorical[date].toFixed(2)
+            };
+            newData.push(item);
+        }
+        //update chart
+        // console.log(newData);
+        bitcoinChart.data.datasets[0].data = newData;
+        bitcoinChart.update();
+        bitcoinChart.resize();
+    });
+});
+
+function formatNumber(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var bitcoinPrice = 0;
+
+// setup chart.js
+
+var ctx = document.getElementById("bitcoin-chart");
+
+
+Chart.defaults.global.animation = {
+    duration: 0
+};
+Chart.defaults.global.legend = {
+    display: false
+};
+Chart.defaults.global.maintainAspectRatio = false;
+
+var bitcoinChart = new Chart(ctx, {
+    type: 'line',
+    backgroundColor: 'rgba(255,255,255,0.3)',
+    data: {
+        datasets: [{
+            label: 'BTC/USD',
+            lineTension: 0,
+            data: [],
+            backgroundColor: 'rgba(237,160,73,0.6)',
+            borderColor: 'rgba(237,160,73,1)',
+            pointRadius: 0,
+        }]
+    },
+    options: {
+        animation: false,
+        tooltips: {
+            backgroundColor: 'rgba(77,77,77,1)',
+            enabled: true,
+            mode: 'nearest',
+            intersect: false,
+            titleFontFamily: 'Ubuntu',
+            bodyFontFamily: 'Ubuntu',
+            displayColors: false,
+            bodyFontColor: '#ccc',
+            titleFontColor: '#eda049',
+            xPadding: 20,
+            caretSize: 10,
+            callbacks: {
+                // tooltipItem is an object containing some information about the item that this label is for (item that will show in tooltip).
+                // data : the chart data item containing all of the datasets
+                label: function (tooltipItem, data) {
+                    console.log('Label-tooltipItem', tooltipItem);
+                    console.log('Label-data', data);
+                    return moment(tooltipItem.xLabel).format('MMM Do, YYYY');
+                    // Return string from this function. You know the datasetIndex and the data index from the tooltip item. You could compute the percentage here and attach it to the string.
+                },
+                title:function (tooltipItem, data) {
+                    console.log('Title-tooltipItem', tooltipItem);
+                    console.log('Title-data', data);
+                    return '$'+formatNumber(parseFloat(tooltipItem[0].yLabel).toFixed(2));
+
+                    // Return string from this function. You know the datasetIndex and the data index from the tooltip item. You could compute the percentage here and attach it to the string.
+                }
+            }
+        },
+        scales: {
+            yAxes: [{
+                display: false
+            }],
+            xAxes: [{
+                type: 'time',
+                position: 'bottom',
+                time: {
+                    displayFormats: {
+                        quarter: 'MMM YYYY'
+                    }
+                },
+                display: false
+            }]
+        }
+    }
+});
